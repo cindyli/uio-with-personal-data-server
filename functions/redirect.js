546959ea7375bdf75744d9a1369dec996726c87a
Cindy Qi Li <cli@ocadu.ca>
2022-07-06 12:10:17 -0400

    fix: fix the issue that netlify auto preserves existing query strings

--- functions/redirect.js
+++ functions/redirect.js
@@ -16,8 +16,11 @@ exports.handler = function(event, context, callback) {
             })
         });
     } else {
+        // Adding a query string of "?a=a" is to work around the issue that netlify auto preserves existing
+        // event.queryStringParameters in the next redirect url even though this is not desired. The work
+        // around is to add another parameter.
+        // See https://answers.netlify.com/t/redirect-doesnt-remove-query-params/31548/9    
         const redirectUrl = refererUrl + "?a=a";
-        console.log("=== redirectUrl: ", redirectUrl);
         return callback(null, {
             statusCode: 302,
             headers: {
